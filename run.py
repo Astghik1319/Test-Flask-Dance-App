de30ad977635c8050f893e0f388f971e6059a2d7
Astghik1319 <74616021+Astghik1319@users.noreply.github.com>
2021-06-21 19:01:08 +0200

    Add files via upload

---
+++ run.py
@@ -0,0 +1,97 @@
+from flask import Flask, redirect, url_for
+from flask import render_template
+from flask import request
+from flask import abort, send_from_directory
+from flask import Flask, render_template, request
+from AzureDB import AzureDB
+from datetime import datetime
+from flask_dance.contrib.github import make_github_blueprint, github
+import secrets
+import os
+
+import os
+app = Flask(__name__)
+
+app.secret_key = secrets.token_hex(16)
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
+
+github_blueprint = make_github_blueprint(
+    client_id="7941ccec80db435ead8f",
+    client_secret="ba8840c912e0c9a5b51444a8dff0bd01fc236846"
+)
+app.register_blueprint(github_blueprint, url_prefix="/login")
+
+@app.route('/')
+def github_login():
+    if not github.authorized:
+        return redirect(url_for("github.login"))
+    resp = github.get("/user")
+    assert resp.ok
+    return home()
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/gallery')
+def gallery():
+    return render_template('gallery.html')
+
+
+@app.route('/aboutme')
+def aboutme():
+    return render_template('aboutme.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+
+@app.route('/user/<username>', methods=['GET', 'POST'])
+def show_user_profile(username):
+    if request.method == 'POST':
+        return 'HTTP post user %s password %s' % (username, request.form['password'])
+    else:
+        return 'HTTP get for user %s' % username
+
+
+@app.route('/error_denied')
+def error_denied():
+    abort(401)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'yourId@gmail.com'
+app.config['MAIL_PASSWORD'] = '*****'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
+@app.route("/")
+def index():
+ msg = Message('Hello', sender = 'yourId@gmail.com', recipients = ['someone1@gmail.com'])
+ msg.body = "Hello Flask message sent from Flask-Mail"
+ mail.send(msg)
+ return "Sent"
+if __name__ == '__main__':
+ app.run(debug = True)
+ 
+ @app.route('/')
+def hello():
+ with AzureDB() as a:
+ data = a.azureGetData()
+ return render_template("result.html", data = data)
+if __name__ == '__main__':
+ app.run(debug=True)
